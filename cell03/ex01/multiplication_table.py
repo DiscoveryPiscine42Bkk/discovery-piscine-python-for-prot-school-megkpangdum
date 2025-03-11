fdecfbc1b3b1e615107f020e3f67e871f09460eb
megkpangdum <41323@prot.ac.th>
2025-03-11 13:47:21 +0700

    Delete cell03/ex01/multiplication_table.py

--- cell03/ex01/multiplication_table.py
+++
@@ -1,6 +0,0 @@
-def multiplication_table(number):
-    print(f"multiplication {number}")
-    for i in range(1, 13):
-        print(f"{number} x {i} = {number * i}")
-number = int(input("Enter the number: "))
-multiplication_table(number)
