4ef02d0d1d17795696c88d0d9cdee56f686ad4af
megkpangdum <41323@prot.ac.th>
2025-03-11 11:51:28 +0700

    Add files via upload

---
+++ cell04/ex03/float.py
@@ -0,0 +1,9 @@
+user_input = input("Give me a number: ")
+try:
+    number = float(user_input)
+    if number.is_integer():
+        print("This number is an integer.")
+    else:
+        print("This number is a decimal.")
+except ValueError:
+    print("The input is not a valid number.")
