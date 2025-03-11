be1d1d82b149cd275e800ec63cc4079adb232de1
megkpangdum <41323@prot.ac.th>
2025-03-11 11:34:52 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+age = int(input("Please tell me your age: "))
+age_in_10_years = age + 10
+age_in_20_years = age + 20
+age_in_30_years = age + 30 
+
+print(f"In 10 years, you'll be {age_in_10_years} years old")
+print(f"In 20 years, you'll be {age_in_20_years} years old")
+print(f"In 30 years, you'll be {age_in_30_years} years old")
